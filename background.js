abb36b0793e142d38c73c8379bd53a740022c8de
Markus Stasko <stasko.markus@gmail.com>
2021-11-13 10:24:38 -0800

    Detect bangs at beginning or end of query

--- background.js
+++ background.js
@@ -6,35 +6,100 @@ let bangs = [];
   bangs = await res.json();
 })();
 
-chrome.webRequest.onBeforeRequest.addListener(({ url }) => {
-  // Get search query from URL
-  const urlObj = new URL(url);
-  const query = urlObj.searchParams.get('q');
+chrome.webRequest.onBeforeRequest.addListener(
+  ({ url }) => {
+    // Get search query from URL
+    const urlObj = new URL(url);
+    const query = urlObj.searchParams.get("q");
+    const searchTerms = query.split(" ");
 
-  // Check if searching for a bang
-  if (query && query.startsWith('!')) {
-    // Redirect to DuckDuckGo
-    return {
-      redirectUrl: `https://www.duckduckgo.com/?q=${encodeURIComponent(query)}`,
-    };
-  }
-}, {
-  urls: [
-    "http://www.google.com/search*", "http://www.google.co.jp/search*", "http://www.google.co.uk/search*", "http://www.google.es/search*", "http://www.google.ca/search*", "http://www.google.de/search*", "http://www.google.it/search*", "http://www.google.fr/search*", "http://www.google.com.au/search*", "http://www.google.com.tw/search*", "http://www.google.nl/search*", "http://www.google.com.br/search*", "http://www.google.com.tr/search*", "http://www.google.be/search*", "http://www.google.com.gr/search*", "http://www.google.co.in/search*", "http://www.google.com.mx/search*", "http://www.google.dk/search*", "http://www.google.com.ar/search*", "http://www.google.ch/search*", "http://www.google.cl/search*", "http://www.google.at/search*", "http://www.google.co.kr/search*", "http://www.google.ie/search*", "http://www.google.com.co/search*", "http://www.google.pl/search*", "http://www.google.pt/search*", "http://www.google.com.pk/search*", "https://www.google.com/search*", "https://www.google.co.jp/search*", "https://www.google.co.uk/search*", "https://www.google.es/search*", "https://www.google.ca/search*", "https://www.google.de/search*", "https://www.google.it/search*", "https://www.google.fr/search*", "https://www.google.com.au/search*", "https://www.google.com.tw/search*", "https://www.google.nl/search*", "https://www.google.com.br/search*", "https://www.google.com.tr/search*", "https://www.google.be/search*", "https://www.google.com.gr/search*", "https://www.google.co.in/search*", "https://www.google.com.mx/search*", "https://www.google.dk/search*", "https://www.google.com.ar/search*", "https://www.google.ch/search*", "https://www.google.cl/search*", "https://www.google.at/search*", "https://www.google.co.kr/search*", "https://www.google.ie/search*", "https://www.google.com.co/search*", "https://www.google.pl/search*", "https://www.google.pt/search*", "https://www.google.com.pk/search*"
-  ],
-}, [
-  "blocking" // Needed so we can redirect the request to DuckDuckGo - we aren't really blocking anything
-]);
+    // Check if searching for a bang
+    if (
+      (searchTerms && searchTerms[0].startsWith("!")) ||
+      searchTerms[searchTerms.length - 1].startsWith("!")
+    ) {
+      // Redirect to DuckDuckGo
+      return {
+        redirectUrl: `https://www.duckduckgo.com/?q=${encodeURIComponent(
+          query
+        )}`,
+      };
+    }
+  },
+  {
+    urls: [
+      "http://www.google.com/search*",
+      "http://www.google.co.jp/search*",
+      "http://www.google.co.uk/search*",
+      "http://www.google.es/search*",
+      "http://www.google.ca/search*",
+      "http://www.google.de/search*",
+      "http://www.google.it/search*",
+      "http://www.google.fr/search*",
+      "http://www.google.com.au/search*",
+      "http://www.google.com.tw/search*",
+      "http://www.google.nl/search*",
+      "http://www.google.com.br/search*",
+      "http://www.google.com.tr/search*",
+      "http://www.google.be/search*",
+      "http://www.google.com.gr/search*",
+      "http://www.google.co.in/search*",
+      "http://www.google.com.mx/search*",
+      "http://www.google.dk/search*",
+      "http://www.google.com.ar/search*",
+      "http://www.google.ch/search*",
+      "http://www.google.cl/search*",
+      "http://www.google.at/search*",
+      "http://www.google.co.kr/search*",
+      "http://www.google.ie/search*",
+      "http://www.google.com.co/search*",
+      "http://www.google.pl/search*",
+      "http://www.google.pt/search*",
+      "http://www.google.com.pk/search*",
+      "https://www.google.com/search*",
+      "https://www.google.co.jp/search*",
+      "https://www.google.co.uk/search*",
+      "https://www.google.es/search*",
+      "https://www.google.ca/search*",
+      "https://www.google.de/search*",
+      "https://www.google.it/search*",
+      "https://www.google.fr/search*",
+      "https://www.google.com.au/search*",
+      "https://www.google.com.tw/search*",
+      "https://www.google.nl/search*",
+      "https://www.google.com.br/search*",
+      "https://www.google.com.tr/search*",
+      "https://www.google.be/search*",
+      "https://www.google.com.gr/search*",
+      "https://www.google.co.in/search*",
+      "https://www.google.com.mx/search*",
+      "https://www.google.dk/search*",
+      "https://www.google.com.ar/search*",
+      "https://www.google.ch/search*",
+      "https://www.google.cl/search*",
+      "https://www.google.at/search*",
+      "https://www.google.co.kr/search*",
+      "https://www.google.ie/search*",
+      "https://www.google.com.co/search*",
+      "https://www.google.pl/search*",
+      "https://www.google.pt/search*",
+      "https://www.google.com.pk/search*",
+    ],
+  },
+  [
+    "blocking", // Needed so we can redirect the request to DuckDuckGo - we aren't really blocking anything
+  ]
+);
 
 // Omnibox Auto-Complete
 chrome.omnibox.onInputChanged.addListener((text, addSuggestions) => {
   let filterText = text.trim();
   if (filterText.indexOf("!") === 0) {
-      filterText = filterText.substr(1)
+    filterText = filterText.substr(1);
   }
 
   chrome.omnibox.setDefaultSuggestion({
-    description: 'Use Bang'
+    description: "Use Bang",
   });
 
   let results = [];
@@ -46,8 +111,8 @@ chrome.omnibox.onInputChanged.addListener((text, addSuggestions) => {
       bang.d.includes(filterText)
     ) {
       // Shorter Bang => More relevant
-      let lengthRelevance = (10 - bang.t.length);
-      if (lengthRelevance > 0){
+      let lengthRelevance = 10 - bang.t.length;
+      if (lengthRelevance > 0) {
         lengthRelevance = 0;
       }
 
@@ -56,7 +121,7 @@ chrome.omnibox.onInputChanged.addListener((text, addSuggestions) => {
       if (bang.t === filterText) {
         // Exact match - set as default suggestion
         chrome.omnibox.setDefaultSuggestion({
-          description: `!${bang.t} (${bang.s})`
+          description: `!${bang.t} (${bang.s})`,
         });
         continue;
       } else if (bang.t.startsWith(filterText)) {
@@ -76,12 +141,15 @@ chrome.omnibox.onInputChanged.addListener((text, addSuggestions) => {
   }
 
   // Sort based on relevance
-  results = results.sort((a, b) => b.relevance - a.relevance).slice(0, 10).map((item) => {
-    return {
-      content: item.content,
-      description: item.description,
-    };
-  });
+  results = results
+    .sort((a, b) => b.relevance - a.relevance)
+    .slice(0, 10)
+    .map((item) => {
+      return {
+        content: item.content,
+        description: item.description,
+      };
+    });
 
   addSuggestions(results);
 });
@@ -89,20 +157,20 @@ chrome.omnibox.onInputChanged.addListener((text, addSuggestions) => {
 chrome.omnibox.onInputEntered.addListener((text, disposition) => {
   let bang = text.trim();
   if (bang.indexOf("!") === 0) {
-      bang = bang.substr(1)
+    bang = bang.substr(1);
   }
 
   const url = `https://www.duckduckgo.com/?q=${encodeURIComponent(`!${bang}`)}`;
 
   switch (disposition) {
     case "currentTab":
-      chrome.tabs.update({url});
+      chrome.tabs.update({ url });
       break;
     case "newForegroundTab":
-      chrome.tabs.create({url});
+      chrome.tabs.create({ url });
       break;
     case "newBackgroundTab":
-      chrome.tabs.create({url, active: false});
+      chrome.tabs.create({ url, active: false });
       break;
   }
 });
